f1b85285098d8515bcdda966723314baba0537c6
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-07-26 15:15:36 +0200

    fix on task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  // Appeler les fonctions signUpUser et uploadPhoto
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  // Utiliser Promise.allSettled pour obtenir les résultats
+  const results = await Promise.allSettled([signUpPromise, uploadPromise]);
+
+  // Formater les résultats selon la spécification
+  return results.map((result) => ({
     status: result.status,
     value: result.value || result.reason,
-  })));
+  }));
 }
