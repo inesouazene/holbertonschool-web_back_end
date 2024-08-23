8d55412cd69e644f3a239377054a05af37d6b661
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-08-23 10:27:11 +0200

    add type: ignore on task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """Yield a random number between 0 and 10 every second, 10 times."""
     for _ in range(10):
         await asyncio.sleep(1)
