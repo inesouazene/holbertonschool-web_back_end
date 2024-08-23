4a7252eb8b47555e334bad434cfd33e6a6a24148
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-08-23 10:14:22 +0200

    update task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""Async generator module."""
 
 import asyncio
 import random
 
 
 async def async_generator():
+    """Yield a random number between 0 and 10 every second, 10 times."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
