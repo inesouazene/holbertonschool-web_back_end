40400580d2f480b2caee80dbedb06d4ed559013f
inesouazene <inesouazene@gmail.com>
2025-06-20 16:35:51 +0200

    resolved pycodestyle problems in files : app.py and index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -32,4 +33,3 @@ def unauthorized() -> str:
       - 401 Unauthorized error
     """
     abort(401, description="Unauthorized access")
-    
