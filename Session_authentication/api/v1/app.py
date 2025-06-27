0b585028c823e446d962b8c5d91d4773135e5521
inesouazene <inesouazene@gmail.com>
2025-06-27 23:37:16 +0200

    update file

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -46,15 +46,16 @@ def before_request() -> None:
         return   # If not required, skip authentication checks
 
     # Check if the Authorization header or session cookie is present
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401, description="Unauthorized access")
+
+    # Then check if the current user is authenticated
+    if auth.current_user is None:
+        abort(403, description="Forbidden access")
 
     # Assign the result of current_user to the request FIRST
     request.current_user = auth.current_user(request)
-
-    # Then check if the current user is authenticated
-    if request.current_user is None:
-        abort(403, description="Forbidden access")
 
 
 @app.errorhandler(401)
