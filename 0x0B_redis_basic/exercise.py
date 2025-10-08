42fd5754088fdeeb08dcb2cf20b7e6aeb72fa721
inesouazene <inesouazene@gmail.com>
2025-10-08 23:52:22 +0200

    added replay function for last task

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -49,6 +49,37 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """
+    Affiche l'historique des appels d'une méthode
+    """
+    redis_instance = method.__self__._redis
+    # créer les clés pour les entrées et sorties
+    method_name = method.__qualname__
+    input_key = f"{method_name}:inputs"
+    output_key = f"{method_name}:outputs"
+
+    # récupérer le nombre d'appels
+    call_count = redis_instance.get(method_name)
+
+    if call_count is None:
+        call_count = 0
+    else:
+        call_count = int(call_count)
+
+    print(f"{method_name} was called {call_count} times:")
+
+    # récupérer les entrées et sorties
+    inputs = redis_instance.lrange(input_key, 0, -1)
+    outputs = redis_instance.lrange(output_key, 0, -1)
+
+    # parcourir les entrées et sorties et les afficher
+    for inp, out in zip(inputs, outputs):
+        input_str = inp.decode('utf-8')
+        output_str = out.decode('utf-8')
+        print(f"{method_name}(*{input_str}) -> {output_str}")
+
+
 class Cache:
     """
     Classe Cache pour stocker des données dans Redis
