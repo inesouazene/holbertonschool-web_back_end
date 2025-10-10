125a3f05666028ab2eef569ad1a81594388ae0a3
inesouazene <inesouazene@gmail.com>
2025-10-10 14:51:31 +0200

    initial commit with README.md and task 0

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- script that creates a table users following some requirements
+-- query that creates a table users with the following columns:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+CREATE TABLE users IF NOT EXISTS (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
