cb329d642da9f4f64c2e15c4b7206ff629b6a339
inesouazene <inesouazene@gmail.com>
2026-02-06 16:31:05 +0100

    update task 9 with if not exists

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- create an index on the first letter of name and the score
-CREATE INDEX idx_first_letter_name_score ON names (name(1), score);
+CREATE INDEX IF NOT EXISTS idx_first_letter_name_score ON names (name(1), score);
